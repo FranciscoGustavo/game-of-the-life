e0d4e6edbf2f3cc2747cc51cc3f9d22bf3121a9c
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-06 11:54:30 -0500

    [Add] Agent

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,40 @@
+class Agent {
+  constructor() {
+    this.board = board;
+    this.rows = rows
+    this.columns = columns
+    this.x = x;
+    this.y = y;
+    this.state = state;
+    this.nextSatate = this.state;
+    this.neighbors = [];
+    this.addNeighbors = this.addNeighbors.bind(this);
+  }
+
+  addNeighbors() {
+    let neighborX;
+    let neighborY;
+
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        neighborX = (this.x + j + this.columns) % this.columns;
+        neighborX = (this.y + i + this.rows) % this.rows;
+
+        if (i !== 0 || j !== 0) {
+          this.neighbors.push(this.board[neighborY][neighborX]);
+        }
+      }
+    }
+  }
+
+  
+}
+
 class LifeGame {
 
   canvas;
   ctx;
+  board
 
   constructor(canvas, width, height, rows, columns) {
     this.canvas = canvas;
@@ -13,8 +46,20 @@ class LifeGame {
     this.sizeCellX = Math.floor(canvasX / rows);
     this.sizeCellY = Math.floor(canvasX / columns);
 
+    this.board = this.createMatrix2D(rows, columns);
+    console.log(this.board);
+    
 
     this.playing = this.playing.bind(this);
+  }
+
+  createMatrix2D(row,column){
+    const matrix = new Array(row);
+    for(let i = 0; i < row; i++){
+      matrix[i]= new Array(column);
+    }
+  
+    return matrix;
   }
 
   playing() {
