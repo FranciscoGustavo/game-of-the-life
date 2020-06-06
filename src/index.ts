83b9231f7f885ecb6f8fd126b66e6a2355ceb910
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-06 11:32:55 -0500

    [Add] initialize game

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,52 @@
-function hello() {
-  console.log('Hello World');
+class LifeGame {
+
+  canvas;
+  ctx;
+
+  constructor(canvas, width, height, rows, columns) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+
+    this.canvas.width = width;
+    this.canvas.height = height
+
+    this.sizeCellX = Math.floor(canvasX / rows);
+    this.sizeCellY = Math.floor(canvasX / columns);
+
+
+    this.playing = this.playing.bind(this);
+  }
+
+  playing() {
+    this.cleanScreen();
+    this.draw();
+  }
+
+  cleanScreen() {
+    this.canvas.width = this.canvas.width;
+    this.canvas.height = this.canvas.height;
+  }
+
+  draw() {
+
+  }
+
+  start() {
+    setInterval(this.playing, 1000 / 5);
+  }
 }
 
-hello();
+const canvas = document.getElementById('life-game');
+const fps = 30;
+
+const canvasX = 500;
+const canvasY = 500;
+
+const rows = 100;
+const columns = 100;
+
+const white = '#FFFFFF';
+const black = '#000000';
+
+const lifeGame = new LifeGame(canvas, canvasX, canvasY, rows, columns);
+lifeGame.start();
